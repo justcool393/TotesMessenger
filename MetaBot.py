6c525ed8fa305975d457e208881008a4c14c54db
justcool393 <wayhay53@gmail.com>
2015-01-15 18:41:34 -0800

    A bunch of changes

--- MetaBot.py
+++ MetaBot.py
@@ -1,57 +1,81 @@
 import logging, os, praw, re, time, sys;
 
-archived = [];
+linked = [];
 user = os.environ['REDDIT_USER'];
+test_reddits = ["TMTest", "Test", "justcool393"];
+blacklist = ["anime", "asianamerican", "askhistorians", "askscience", "askreddit", "aww", "chicagosuburbs", "benfrick",
+             "bmw", "cosplay", "cumberbitches", "d3gf", "deer", "depression", "depthhub", "drinkingdollars",
+             "forwardsfromgrandma", "futurology", "geckos", "giraffes", "graphical_design", "grindsmygears",
+             "indianfetish", "misc", "mixedbreeds", "news", "newtotf2", "omaha", "petstacking", "pigs",
+             "politicaldiscussion", "politics", "programmingcirclejerk", "raerthdev", "rants", "salvia", "science",
+             "seiko", "shoplifting", "sketches", "sociopath", "suicidewatch", "talesfromtechsupport", "unitedkingdom"];
 
 
 def main():
     r = praw.Reddit("Links to reddit posts from other places in reddit", domain="api.reddit.com");
     r.login(user, os.environ['REDDIT_PASS']);
-    logging.info("Logged in and started post archiving.");
-    s = r.get_subreddit('justcool393');
+    logging.info("Logged in and started linking");
 
     check_at = 3600;
     last_checked = 0;
     times_zero = 1;
 
-    linked = archive_submissions(r, s, 100, 15);
+    count = link_subs(r, 100, 15);
     # Check the last 100 posts on startup
     while True:
         if time.time() - last_checked > check_at:
             last_checked = time.time();
-            if linked == 0:
+            if count == 0:
                 pass;
             else:
-                logging.info("Linked " + str(linked) + " in the last " + str((check_at * times_zero) / 60) + " minutes");
-                linked = 0;
+                logging.info("Linked " + str(count) + " in the last " + str((check_at * times_zero) / 60) + " minutes");
+                count = 0;
                 times_zero = 1;
 
-        linked += archive_submissions(r, s, 50, 120);
+        count += link_subs(r, 50, 45);
 
 
-def archive_submissions(r, s, count, delay):
-    archived_posts = 0;
-    for submission in s.get_new(limit=count):
+def link_subs(r, count, delay):
+    linked_count = 0;
+    for submission in r.get_domain_listing('reddit.com', sort='new', limit=count):
 
-        submission.replace_more_comments(limit=None, threshold=0);
-
-        if submission.id in archived:
+        if submission.subreddit not in test_reddits:  # For testing things
             continue;
 
-        commented = check_commented(submission);
-
-        if commented:
-            archived.append(submission.id);
+        if not is_comment(submission.url):
             continue;
 
         try:
-            archive_and_post(r, submission);
-            archived_posts += 1;
-        except UnicodeEncodeError:
-            logging.error("Unable to archive post (UnicodeEncodeError, Submission ID: " + submission.id + ")");
+            linkedp = get_linked(r, submission.url);
+        except praw.errors.ClientException:
+            logging.error("Link is not a reddit post! (ID: " + submission.id + ")");
+            continue;
+
+        if linkedp.id in linked:
+            continue;
+
+        if linkedp.subreddit in blacklist:
+            linked.append(linkedp);
+            continue;  # Do not comment in blacklisted subreddits (reddit rules)
+
+        # TODO: Make the bot edit it's comment on other links.
+        commented = check_commented(linkedp);
+
+        if commented:
+            linked.append(linkedp);
+            continue;
+
+        # End to do
+
+        post(r, linkedp, submission);  # Hope it works
 
     time.sleep(delay);
-    return archived_posts;
+    return linked_count;
+
+
+def get_linked(r, link):
+    return r.get_submission(link);
+
 
 def check_commented(s):
     flat_comments = praw.helpers.flatten_tree(s.comments);
@@ -64,44 +88,58 @@ def check_commented(s):
             return True;
     return False;
 
-def archive_and_post(r, s):
-    post(r, s, arch_post);
 
-
-def post(r, s, link):
+def post(r, s, post):
     comment = """
-This post has been linked to from another place on reddit. ([Info](/r/)
+This post has been linked to from another place on reddit. ([Info](/r/TotesMessenger/wiki/))
 
 {{link}}""";
     try:
-        s.add_comment(comment.format(link=''));
+        s.add_comment(comment.format(link=format_link(post)), raise_captcha_exception=True);
+    except praw.errors.InvalidCaptcha:
+        logging.error("Cannot comment on post (comment karma is too low)");
     except Exception as e:
         logging.error("Exception on comment add! (Submission ID: " + str(s.id) + ")");
         logging.error(str(e));
         pass;
 
+
 def format_link(post):
     link = post.url;
-    return "- [" + get_subreddit(link) + "] " + "[" + post.title + "](" + np(link) + ")\n";
+    return "- [/r/" + post.subreddit + "] " + "[" + post.title + "](" + np(link) + ")\n";
+
 
 def np(link):
     return re.sub(r"//[a-z]{0,3}\.?reddit", "//np.reddit", link);
 
-def get_subreddit(link):
-    reg = re.sub("http[s]?://.{0,}reddit.com", "", link);
-    return re.sub("\/c.*", "", reg);
+
+def is_comment(link):
+    a = re.compile("\/r\/.{1,20}\/comments/.*");
+    return a.match(link);
+
 
 def log_crash(e):
     logging.error("Error occurred in the bot; restarting in 15 seconds...");
     logging.error("Details: " + str(e));
     time.sleep(15);
-    sys.exit(1); # Signal to the host that we crashed
+    sys.exit(1);  # Signal to the host that we crashed
+
+
+def setup_logging():
+    root = logging.getLogger();
+    root.setLevel(logging.INFO);
+
+    ch = logging.StreamHandler(sys.stdout);
+    ch.setLevel(logging.INFO);
+
+    root.addHandler(ch);
 
 
 try:
+    setup_logging();
     main();
 except (NameError, SyntaxError) as e:
     logging.error(str(e));
-    time.sleep(86400); # Sleep for 1 day so we don't restart.
+    time.sleep(86400);  # Sleep for 1 day so we don't restart.
 except Exception as e:
     log_crash(e);
