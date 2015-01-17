793622a793a57bc7887738734bad0efa0a3f456c
justcool393 <wayhay53@gmail.com>
2015-01-16 23:10:26 -0800

    Test enabled

--- MetaBot.py
+++ MetaBot.py
@@ -53,8 +53,9 @@ def link_subs(r, count, delay):
     linked_count = 0;
     for submission in r.get_domain_listing('reddit.com', sort='new', limit=count):
 
-        #if submission.subreddit.display_name.lower() not in test_reddits:  # For testing things
-        #    continue;
+        if submission.subreddit.display_name.lower() not in test_reddits:  # For testing things
+            continue;
+            
         url = submission.url;
         if not is_comment(url):
             continue;
