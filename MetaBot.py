51047e02d67d183292665b7133bbaa52f03e9179
justcool393 <wayhay53@gmail.com>
2015-01-17 00:19:01 -0800

    ...

--- MetaBot.py
+++ MetaBot.py
@@ -246,5 +246,5 @@ try:
 except (NameError, SyntaxError) as e:
     logging.error(exi(e));
     time.sleep(86400);  # Sleep for 1 day so we don't restart.
-except Exception as e:
-    log_crash(e);
+#except Exception as e:
+#    log_crash(e);
