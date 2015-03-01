e19a983d55c7c840909de8b63fe0288ea0700ce2
justcool393 <wayhay53@gmail.com>
2015-03-01 14:27:36 -0800

    Fuck this shit...

--- MetaBot.py
+++ MetaBot.py
@@ -62,7 +62,7 @@ def link_subs(r, count, delay):
 
         try:
             link_submission(submission);
-        except HTTPError as e:
+        except Exception as e:
             logging.error(exi(e));
         linked_count += 1;
         time.sleep(3);
